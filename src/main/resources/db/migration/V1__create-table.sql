22d3d448ab57473240986fcf06f7f4367fecc778
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2023-06-14 06:14:47 -0300

    feat: incrementando mock inicial

--- src/main/resources/db/migration/V1__create-table.sql
+++ src/main/resources/db/migration/V1__create-table.sql
@@ -30,10 +30,13 @@ CREATE TABLE car_rental (
 );
 
 -- mock
-INSERT INTO car (plate, model, brand, current_km, vehicle_year) VALUES ( 'MODELO1', 'GOL', 'VOLKSWAGEN', 2000.0, '2012-01-01');
-INSERT INTO car ( plate, model, brand, current_km, vehicle_year) VALUES ('MODELO2', 'HB20', 'HYUNDAI', 2000.0, '2015-01-01');
-INSERT INTO car (plate, model, brand, current_km, vehicle_year) VALUES ('MODELO3', 'FOX', 'VOLKSWAGEN', 2000.0, '2010-01-01');
-INSERT INTO car (plate, model, brand, current_km, vehicle_year) VALUES ('MODELO4', 'RENEGADE', 'JEEP', 2000.0, '2020-01-01');
+INSERT INTO car (plate, model, brand, current_km, vehicle_year) VALUES ( 'MODELO1', 'GOL', 'VOLKSWAGEN', 18900.53, '2012-01-01');
+INSERT INTO car ( plate, model, brand, current_km, vehicle_year) VALUES ('MODELO2', 'HB20', 'HYUNDAI', 1239.14, '2015-01-01');
+INSERT INTO car (plate, model, brand, current_km, vehicle_year) VALUES ('MODELO3', 'FOX', 'VOLKSWAGEN', 26835.12, '2010-01-01');
+INSERT INTO car (plate, model, brand, current_km, vehicle_year) VALUES ('MODELO4', 'RENEGADE', 'JEEP', 17998.12, '2020-01-01');
+INSERT INTO car (plate, model, brand, current_km, vehicle_year) VALUES ('MODELO5', 'COMPASS', 'JEEP', 998.2, '2022-01-01');
 
-INSERT INTO client (id, name, email, cpf, phone) VALUES (1, 'Eduardo Valencio', 'eduardo.valencio.santos@gmail.com', '123.123.123-43', '14998989889');
-INSERT INTO client (id, name, email, cpf, phone) VALUES (2, 'Leonardo Valencio', 'leonardo.valencio@gmail.com', '223.123.123-43', '149989089889');
+INSERT INTO client (id, name, email, cpf, phone) VALUES (1, 'Eduardo Valencio', 'eduardo.valencio.santos@gmail.com', '123.123.123-41', '2342342234234');
+INSERT INTO client (id, name, email, cpf, phone) VALUES (2, 'Leonardo Valencio', 'leonardo.valencio@gmail.com', '223.123.123-42', '123123123124');
+INSERT INTO client (id, name, email, cpf, phone) VALUES (3, 'Fabiano Valencio', 'fabiano.valencio@gmail.com', '223.123.123-43', '78912374612');
+INSERT INTO client (id, name, email, cpf, phone) VALUES (4, 'Maria Aparecida', 'maria.aparecida@gmail.com', '223.123.123-44', '12678634162');
